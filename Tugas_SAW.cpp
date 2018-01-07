6c8ab90a4c4d3edf804b2216b2ead1043c41c060
shintiasari <32458115+shintiasari@users.noreply.github.com>
2018-01-07 11:49:57 +0800

    Membuat setting presisi desimal

---
+++ Tugas_SAW.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	
+	// SETTING PRESISI DESIMAL
+	cout.setf(ios::fixed);
+	cout.setf(ios::showpoint);
+	cout.precision(4);
